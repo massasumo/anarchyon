9f68ae395eabde5a534216cd86acf6f4ad278e4d
massasumo <massasumo3@gmail.com>
2026-08-05 18:51:43 -0300

    Fix: mudando versoes do server 3

--- start.sh
+++ start.sh
@@ -29,25 +29,25 @@ download_file "fabric-api" "https://cdn.modrinth.com/data/P7dR8mSH/versions/xhLT
 # 2. Architectury API (1.20.1)
 download_file "architectury" "https://cdn.modrinth.com/data/lhGA9TYQ/versions/WbL7MStR/architectury-9.2.14-fabric.jar"
 
-# 3. FTB Library (1.20.1 Fabric v2001.2.5)
-download_file "ftb-library" "https://cdn.modrinth.com/data/Ch21O3I3/versions/r3RkXz8M/ftb-library-fabric-2001.2.5.jar"
+# 3. FTB Library (1.20.1 Fabric v2001.2.5 - Mirror Direto .jar)
+download_file "ftb-library" "https://edge.forgecdn.net/files/5460/328/ftb-library-fabric-2001.2.5.jar"
 
-# 4. FTB Teams (1.20.1 Fabric v2001.3.0)
-download_file "ftb-teams" "https://cdn.modrinth.com/data/14Bimfbf/versions/T210fX1P/ftb-teams-fabric-2001.3.0.jar"
+# 4. FTB Teams (1.20.1 Fabric v2001.3.0 - Mirror Direto .jar)
+download_file "ftb-teams" "https://edge.forgecdn.net/files/5460/333/ftb-teams-fabric-2001.3.0.jar"
 
 # 5. PacketEvents (v2.7.0 / Fabric)
 download_file "packetevents" "https://github.com/retrooper/packetevents/releases/download/v2.7.0/packetevents-fabric-2.7.0.jar"
 
-# 6. GrimAC (1.20.1 Fabric v2.3.65)
+# 6. GrimAC (1.20.1 Fabric)
 download_file "grimac" "https://cdn.modrinth.com/data/9eGK36P1/versions/Bms7534q/GrimAC-2.3.65.jar"
 
-# 7. Cristel Lib (1.20.1 v1.1.5)
+# 7. Cristel Lib (1.20.1)
 download_file "cristel-lib" "https://cdn.modrinth.com/data/1e243A26/versions/jR76EwXv/cristellib-fabric-1.1.5.jar"
 
-# 8. Towns and Towers (1.20.1 v1.10.2)
+# 8. Towns and Towers (1.20.1)
 download_file "towns-and-towers" "https://cdn.modrinth.com/data/K3A3423L/versions/K37z0c8T/Towns-and-Towers-1.10.2%2B1.20.1.jar"
 
-# 9. Geyser Fabric (1.20.1 / GeyserMC API)
+# 9. Geyser Fabric (1.20.1)
 download_file "geyser" "https://download.geysermc.org/v2/projects/geyser/versions/2.2.0/builds/380/downloads/fabric"
 
 # 10. Fabric Permissions API (v0.3.1)
