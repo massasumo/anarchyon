2247ffe7e3be5f72ddffb16c85963dd0ea7e07a3
massasumo <massasumo3@gmail.com>
2026-08-05 18:34:05 -0300

    Add: Workflow do Actions e dependencias do Fabric

--- start.sh
+++ start.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-# Garantir que a pasta mods existe
+
+# Criar pasta de mods
 mkdir -p mods
 
-# Baixar o Cobblemon diretamente da CDN do Modrinth se não estiver presente
-COBBLEMON_JAR="mods/Cobblemon-fabric-1.5.2+1.20.1.jar"
-if [ ! -f "$COBBLEMON_JAR" ]; then
-    echo "Baixando o mod Cobblemon (104MB)..."
-    curl -L -o "$COBBLEMON_JAR" "https://cdn.modrinth.com/data/lhGA9vAc/versions/wK15fOag/Cobblemon-fabric-1.5.2%2B1.20.1.jar"
-fi
+echo "Baixando dependencias do Fabric e Cobblemon..."
+# Fabric API
+curl -sL -o mods/fabric-api.jar https://cdn.modrinth.com/data/P7R216yM/versions/K3S2N3gE/fabric-api-0.92.2%2B1.20.1.jar
 
-# Download do Fabric Server Launcher 1.20.1
-if [ ! -f fabric-server-launch.jar ]; then
-    echo "Baixando Fabric Server 1.20.1..."
-    curl -o fabric-server-launch.jar https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.11/1.0.1/server/jar
-fi
+# Architecture API
+curl -sL -o mods/architectury.jar https://cdn.modrinth.com/data/lhA11yRo/versions/v4i3f6X6/architectury-9.2.14-fabric.jar
 
-# Aceitar EULA
+# Cobblemon
+curl -sL -o mods/Cobblemon-fabric-1.5.2+1.20.1.jar https://cdn.modrinth.com/data/1K3C899a/versions/9S7kQJvX/Cobblemon-fabric-1.5.2%2B1.20.1.jar
+
+echo "Aceitando eula..."
 echo "eula=true" > eula.txt
 
-# Iniciar servidor com otimização de RAM (4GB)
-java -Xms2048M -Xmx4096M -jar fabric-server-launch.jar nogui
+echo "Baixando Fabric Server Installer..."
+curl -sL -o fabric-server-launch.jar https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.11/1.0.1/server/jar
+
+echo "Iniciando Servidor Minecraft..."
+java -Xmx6G -Xms2G -jar fabric-server-launch.jar nogui
