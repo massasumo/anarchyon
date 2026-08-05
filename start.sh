8b9da88862edce6db6da7956c7b9740c7a530e06
massasumo <massasumo3@gmail.com>
2026-08-05 19:27:08 -0300

    Fix: corrigindo fabric

--- start.sh
+++ start.sh
@@ -8,13 +8,8 @@ curl -fSL -o mods/Cobblemon-fabric-1.5.2.jar "https://cdn.modrinth.com/data/MdwF
 echo "=== Aceitando EULA ==="
 echo "eula=true" > eula.txt
 
-if [ -f "mods/fabric-installer-1.1.2.jar" ]; then
-    mv mods/fabric-installer-1.1.2.jar ./fabric-installer-1.1.2.jar
-fi
-
-echo "=== Instalando o Servidor Fabric ==="
-# Adicionado a palavra 'server' antes das flags
-java -jar fabric-installer-1.1.2.jar server --mcversion 1.20.1 --loader 0.16.10 --downloadMinecraft
+echo "=== Instalando o Servidor Fabric e baixando o Minecraft Server JAR ==="
+java -jar fabric-installer-1.1.2.jar server -mcversion 1.20.1 -loader 0.16.10 -downloadMinecraft
 
 echo "=== Iniciando o Servidor ==="
 java -Xmx4G -jar fabric-server-launch.jar nogui
