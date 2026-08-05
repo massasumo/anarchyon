95159908ffd9312a4eddd19a2cc44f22d4088ddb
massasumo <massasumo3@gmail.com>
2026-08-05 19:16:21 -0300

    Fix: corrigindo link cobblemon

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
 set -e
+
+mkdir -p mods
+
+echo "=== Baixando Cobblemon 1.5.2 ==="
+curl -fSL -o mods/Cobblemon-fabric-1.5.2.jar "https://cdn.modrinth.com/data/MdwFAVRL/versions/EVozVxCq/Cobblemon-fabric-1.5.2%2B1.20.1.jar"
 
 echo "=== Aceitando EULA ==="
 echo "eula=true" > eula.txt
-
-echo "=== Baixando Fabric Server Installer ==="
-curl -fSL -o fabric-installer.jar https://meta.fabricmc.net/v2/versions/installer/1.0.1/server/jar
 
 echo "=== Instalando e Iniciando o Servidor ==="
 java -Xmx2G -jar fabric-installer.jar --mcversion 1.20.1 --loader 0.16.10 --downloadMinecraft
