f5237e2eb317f83742afa812e0eda9578d742469
massasumo <massasumo3@gmail.com>
2026-08-05 22:53:56 -0300

    Fix: nao ageunto mais funcione

--- start.sh
+++ start.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-set -e
-
-mkdir -p mods
 
 echo "=== Baixando Cobblemon 1.5.2 ==="
-curl -fSL -o mods/Cobblemon-fabric-1.5.2.jar "https://cdn.modrinth.com/data/MdwFAVRL/versions/EVozVxCq/Cobblemon-fabric-1.5.2%2B1.20.1.jar"
+mkdir -p mods
+wget -O mods/Cobblemon-Fabric-1.5.2+1.20.1.jar "https://cdn.modrinth.com/data/lhGA9TYQ/versions/U6x3aPzC/Cobblemon-Fabric-1.5.2%2B1.20.1.jar"
 
 echo "=== Aceitando EULA ==="
 echo "eula=true" > eula.txt
 
 echo "=== Instalando o Servidor Fabric ==="
-java -jar fabric-installer-1.1.2.jar server -mcversion 1.20.1 -loader 0.16.10 -downloadMinecraft
-
-echo "=== Iniciando Túnel Playit ==="
-sudo playit start &
-sleep 5
+wget -O fabric-installer.jar https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.1.2/fabric-installer-1.1.2.jar
+java -jar fabric-installer.jar server -mcversion 1.20.1 -downloadMinecraft
 
 echo "=== Iniciando o Servidor ==="
-java -Xmx4G -jar fabric-server-launch.jar nogui
+java -Xmx6G -Xms2G -jar fabric-server-launch.jar nogui
